692648b8b68fbc6b6995ef8150c14d817df0e7f1
Tristan Lins <tristan.lins@infinitysoft.de>
2010-11-23 16:19:48 +0000

    Remove the array_merge and atomize the single configuration entries.

--- system/modules/xyaml/config/config.php
+++ system/modules/xyaml/config/config.php
@@ -30,37 +30,31 @@
 #$GLOBALS['TL_HOOKS']['outputFrontendTemplate'][] = array('xYAML', 'outputFrontendTemplate');
 $GLOBALS['TL_HOOKS']['replaceInsertTags'][] = array('NewsletterYAMLInsertTags', 'replaceInsertTags');
 $GLOBALS['TL_PTY']['regular'] = 'PageRegularYAML';
-$GLOBALS['xYAML'] = array_merge(
-	array(
-		'absolute_yaml_path' => '/usr/lib/www/yaml',
-	
-		'yaml_css' => array(
-			array('src' => '/_yaml/core/base.css'),
-			array('src' => '/_yaml/core/iehacks.css', 'condition' => 'lte IE 7')
-		),
-		
-		'style_css' => array(
-			array('src' => 'css/basemod.css'),
-			array('src' => 'css/content.css'),
-			array('src' => 'css/print.css', 'media' => 'print'),
-			array('src' => 'css/ie6patches.css', 'condition' => 'IE 6'),
-			array('src' => 'css/ie7patches.css', 'condition' => 'IE 7'),
-			array('src' => 'css/ie8patches.css', 'condition' => 'IE 8')
-		),
-		
-		'newsletter_css' => array(
-			array('src' => 'css/content.css'),
-			array('src' => 'css/newsletter.css')
-		),
-		
-		'wrapper_css_selector' => array('#page_margins'),
-		'header_css_selector' => array('#header'),
-		'left_css_selector' => array('#col1'),
-		'right_css_selector' => array('#col2'),
-		'main_css_selector' => array('#col3'),
-		'footer_css_selector' => array('#footer')
-	),
-	is_array($GLOBALS['xYAML']) ? $GLOBALS['xYAML'] : array());
+
+$GLOBALS['xYAML']['absolute_yaml_path'] = '/usr/lib/www/yaml';
+$GLOBALS['xYAML']['yaml_css'] = array(
+	array('src' => '/_yaml/core/base.css'),
+	array('src' => '/_yaml/core/iehacks.css', 'condition' => 'lte IE 7')
+);
+$GLOBALS['xYAML']['style_css'] = array(
+	array('src' => 'css/basemod.css'),
+	array('src' => 'css/content.css'),
+	array('src' => 'css/print.css', 'media' => 'print'),
+	array('src' => 'css/ie6patches.css', 'condition' => 'IE 6'),
+	array('src' => 'css/ie7patches.css', 'condition' => 'IE 7'),
+	array('src' => 'css/ie8patches.css', 'condition' => 'IE 8')
+);
+$GLOBALS['xYAML']['newsletter_css'] = array(
+	array('src' => 'css/content.css'),
+	array('src' => 'css/newsletter.css')
+);
+$GLOBALS['xYAML']['wrapper_css_selector'] = array('#page_margins');
+$GLOBALS['xYAML']['header_css_selector'] = array('#header');
+$GLOBALS['xYAML']['left_css_selector'] = array('#col1');
+$GLOBALS['xYAML']['right_css_selector'] = array('#col2');
+$GLOBALS['xYAML']['main_css_selector'] = array('#col3');
+$GLOBALS['xYAML']['footer_css_selector'] = array('#footer');
+
 $GLOBALS['BE_MOD']['content']['newsletter']['send'][0] = 'NewsletterYAML';
 
 ?>
