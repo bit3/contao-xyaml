7530829b0649544f85fc7740b2e2341a22a574fa
Tristan Lins <tristan.lins@bit3.de>
2013-11-25 23:47:04 +0100

    Fix "Argument 5 must be an array" issue.

--- src/Bit3/Contao/XYAML/Hooks.php
+++ src/Bit3/Contao/XYAML/Hooks.php
@@ -64,7 +64,7 @@ class Hooks
 					$filters = array(AsseticFactory::createFilterOrChain($filter));
 				}
 				else {
-					$filters = null;
+					$filters = array();
 				}
 
 				// add yaml addons
